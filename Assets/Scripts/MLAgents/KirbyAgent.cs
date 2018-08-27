19464feb986806a9d5a506de2be178346ef09951
Chapa.C(Takuya) <chapatown@gmail.com>
2018-08-27 23:46:04 +0900

    KirbyAgents override準備

--- Assets/Scripts/MLAgents/KirbyAgent.cs
+++ Assets/Scripts/MLAgents/KirbyAgent.cs
@@ -9,5 +9,31 @@ namespace CCG
 {
     public class KirbyAgent : Agent
     {
+        #region public methods
+        public override void AgentAction(float[] vectorAction, string textAction)
+        {
+            base.AgentAction(vectorAction, textAction);
+        }
+
+        public override void AgentOnDone()
+        {
+            base.AgentOnDone();
+        }
+
+        public override void AgentReset()
+        {
+            base.AgentReset();
+        }
+
+        public override void CollectObservations()
+        {
+            base.CollectObservations();
+        }
+
+        public override void InitializeAgent()
+        {
+            base.InitializeAgent();
+        }
+        #endregion
     }
 }
